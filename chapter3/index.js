918d28e5ccc34c3a4b655bb5362aa8bd03e5b69c
Wataru <tiandaoxiongy1662@gmail.com>
2026-02-23 10:46:46 +0900

    commit message

---
+++ chapter3/index.js
@@ -0,0 +1,6 @@
+function changeText() {
+  const title = document.getElementById("title");
+  title.innerText = "SaaS QA Mode";
+  title.style.color = "red";
+  document.body.style.backgroundColor = "#f5f5f5";
+}
